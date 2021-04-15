9c4ddd098dc6f9866891966837288746889990b7
benwater12 <b05505033@ntu.edu.tw>
2021-04-15 23:42:01 +0800

    Update breaktime.js

--- breaktime.js
+++ breaktime.js
@@ -110,7 +110,6 @@ class BreakTime {
 
         //send message declaring if you're back
         if (userId == game.user._id) {
-            AudioHelper.play({src: ["https://assets.forge-vtt.com/5ffd1ed411fad728bfd1a267/(27s%20-%2030s)%20lobotomy%20corp-Blue%20star%27s%20bgm%20and%20attack%20sound.mp3"]}, true);
             const isaway = BreakTime.awayUsers.has(userId);
             if(isaway){AudioHelper.play({src: ["https://assets.forge-vtt.com/5ffd1ed411fad728bfd1a267/(27s%20-%2030s)%20lobotomy%20corp-Blue%20star%27s%20bgm%20and%20attack%20sound.mp3"]}, true) ;}
             else{AudioHelper.play({src: ["https://assets.forge-vtt.com/5ffd1ed411fad728bfd1a267/Among%20Us%20(Emergency%20Meeting)%20-%20Sound%20Effect%20(HD).mp3"]}, true) ;}
